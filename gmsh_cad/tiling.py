d826e66af6ac54421fbee318ef7deff267c692c0
mirok <miroslav.kuchta@gmail.com>
2018-05-29 20:29:38 +0200

    tiling as script

--- gmsh_cad/tiling.py
+++ gmsh_cad/tiling.py
@@ -2,7 +2,8 @@ from tiling_cpp import (fill_mesh, fill_mesh_function, fill_mesh_valuecollection
                         fill_mf_from_mvc)
 from test_periodic import compute_vertex_periodicity
 
-from dolfin import CompiledSubDomain, Mesh, MeshEditor, MeshFunction, MeshValueCollection
+from dolfin import (CompiledSubDomain, Mesh, MeshEditor, MeshFunction, MeshValueCollection,
+                    Timer, info, SubsetIterator)
 from collections import defaultdict
 from itertools import izip
 import numpy as np
@@ -223,66 +224,99 @@ def as_meshf(mvc, init_value=0):
 
     return mesh_f
 
-# --------------------------------------------------------------------
+
+def load_data(mesh, h5_file, data_set, dim, data):
+    '''
+    Fill the data dictionary with data_set representing mesh function with 
+    dim over mesh read from h5_file according to key spec expected by tiling 
+    algorithm.
+    '''
+    mf = MeshFunction('size_t', mesh, dim, 0)
+    h5_file.read(mf, data_set)
+            
+    # Data to evolve
+    mesh.init(dim, 0)
+    e2v = tile.topology()(dim, 0)
+
+    tags = set(mf.array())
+    # Don't evolve zero - we initialize to it
+    if 0 in tags: tags.remove(0)
+
+    for tag in tags:
+        data[(dim, tag)] = np.array([e2v(e.index()) for e in SubsetIterator(mf, tag)],
+                                    dtype='uintp')
+    return data
+
+# ------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    from dolfin import (File, HDF5File, mpi_comm_world, Mesh, UnitSquareMesh,
-                        Timer, info, SubsetIterator, CellVolume, dx, assemble,
-                        ds, dS, FacetArea, avg, SubsetIterator)
+    from dolfin import mpi_comm_world, HDF5File, Timer, File
+    import argparse, os
 
-    def test(path, type='mf'):
-        '''Evolve the tile in (n, n) pattern checking volume/surface properties'''
+    parser = argparse.ArgumentParser(description='Put n tiles in x axis, m in y axis.')
+    parser.add_argument('tile', type=str, help='H5 file that is the file')
+    parser.add_argument('-n', type=int, default=1)
+    parser.add_argument('-m', type=int, default=1)
+    parser.add_argument('-facet_tags', type=str, default='surfaces',
+                        help='name under which H5 stores facet tags')
+    parser.add_argument('-cell_tags', type=str, default='volumes',
+                        help='name under which H5 stores volume tags')
 
-        comm = mpi_comm_world()
-        h5 = HDF5File(comm, path, 'r')
-        tile = Mesh()
-        h5.read(tile, 'mesh', False)
+    save_pvd_parser = parser.add_mutually_exclusive_group(required=False)
+    save_pvd_parser.add_argument('--save_pvd', dest='save_pvd', action='store_true')
+    save_pvd_parser.add_argument('--no_save_pvd', dest='save_pvd', action='store_false')
+    parser.set_defaults(save_pvd=False)
 
-        init_container = lambda type, dim: (MeshFunction('size_t', tile, dim, 0)
-                                            if type == 'mf' else
-                                            MeshValueCollection('size_t', tile, dim))
-        
-        for n in (2, 4):
-            data = {}
-            checks = {}
-            for dim, name in zip((2, 3), ('surfaces', 'volumes')):
-                # Get the collection
-                collection = init_container(type, dim)
-                h5.read(collection, name)
-                
-                if type == 'mvc': collection = as_meshf(collection)
-            
-                # Data to evolve
-                tile.init(dim, 0)
-                e2v = tile.topology()(dim, 0)
-                # Only want to evolve tag 1 (interfaces) for the facets. 
-                data[(dim, 1)] = np.array([e2v(e.index()) for e in SubsetIterator(collection, 1)],
-                                          dtype='uintp')
-                
-                if dim == 2:
-                    check = lambda m, f: assemble(FacetArea(m)*ds(domain=m, subdomain_data=f, subdomain_id=1)+
-                                                  avg(FacetArea(m))*dS(domain=m, subdomain_data=f, subdomain_id=1))
-                else:
-                    check = lambda m, f: assemble(CellVolume(m)*dx(domain=m, subdomain_data=f, subdomain_id=1))
+    args = parser.parse_args()
 
-                checks[dim] = lambda m, f, t=tile, c=collection, n=n, check=check: abs(check(m, f)-n**2*check(t, c))/(n**2*check(t, c))
+    # Some sanity
+    root, ext = os.path.splitext(args.tile)
+    assert ext == '.h5'
 
-            t = Timer('x')
-            mesh, mesh_data = TileMesh(tile, (n, n), mesh_data=data)
-            info('\tTiling took %g s. Ncells %d, nvertices %d, \n' % (t.stop(), mesh.num_vertices(), mesh.num_cells()))
-            
-            foos = mf_from_data(mesh, mesh_data)
-            # Mesh Functions
-            from_mf = np.array([checks[dim](mesh, foos[dim]) for dim in (2, 3)])
-
-            mvcs = mvc_from_data(mesh, mesh_data)
-            foos = as_meshf(mvcs)
-            # Mesh ValueCollections
-            from_mvc = np.array([checks[dim](mesh, foos[dim]) for dim in (2, 3)])
-
-            assert np.linalg.norm(from_mf - from_mvc) < 1E-13
-            # I ignore shared facets so there is bound to be some error in facets
-            # Volume should match well
-            print from_mf
+    shape = (args.n, args.m)
+    assert all((((v & (v - 1)) == 0) and v > 0) for v in shape)
     
-    test('tile_2x2.h5')
+    # Load the tile mesh
+    comm = mpi_comm_world()
+    h5 = HDF5File(comm, args.tile, 'r')
+    tile = Mesh()
+    h5.read(tile, 'mesh', False)
+
+    data = {}
+    cell_dim = tile.topology().dim()
+    facet_dim = cell_dim - 1
+
+    if args.facet_tags: 
+        data = load_data(tile, h5, args.facet_tags, facet_dim, data)
+    
+    if args.cell_tags: 
+        data = load_data(tile, h5, args.cell_tags, cell_dim, data)
+
+    t = Timer('tile')
+    mesh, mesh_data = TileMesh(tile, shape, mesh_data=data)
+    info('\nTiling took %g s; nvertices %d, ncells %d' % (t.stop(),
+                                                          mesh.num_vertices(),
+                                                          mesh.num_cells()))
+
+    # Saving
+    t = Timer('save')
+    h5_file = '%s_%d_%d.h5' % (root, shape[0], shape[1])
+        
+    out = HDF5File(mesh.mpi_comm(), h5_file, 'w')
+    out.write(mesh, 'mesh')
+    
+    tt = Timer('data')
+    # To mesh functions
+    if mesh_data:
+        mfs = mf_from_data(mesh, mesh_data)
+
+        for dim, name in (zip((facet_dim, cell_dim), (args.facet_tags, args.cell_tags))):
+            if name:
+                out.write(mfs[dim], name)
+            
+                if args.save_pvd:
+                    File('%s_%d_%d_%s.pvd' % (root, shape[0], shape[1], name)) << mfs[dim]
+                
+    info('\t\tGetting data as MeshFoo took %g s' % tt.stop())
+    
+    info('\tSaving took %g' % t.stop())
