91809f8ee0fe636494e25e07bf4a1de2ddb8ddbf
mirok <miroslav.kuchta@gmail.com>
2018-05-30 12:53:34 +0200

    make all the markers binary

--- gmsh_cad/emi_system.py
+++ gmsh_cad/emi_system.py
@@ -6,7 +6,7 @@ parameters['form_compiler']['cpp_optimize'] = True
 parameters['form_compiler']['cpp_optimize_flags'] = '-O3 -ffast-math -march=native'
 parameters['ghost_mode'] = 'shared_facet'
 
-mesh_file = 'tile_1_narrow_2_2.h5'#'tile_2x2.h5'
+mesh_file = 'tile_1_narrow.h5'#'tile_2x2.h5'
 
 comm = mpi_comm_world()
 h5 = HDF5File(comm, mesh_file, 'r')
@@ -15,14 +15,12 @@ h5.read(mesh, 'mesh', False)
 # The mesh comes in micro meters. Below it is more convenient to work in cm
 mesh.coordinates()[:] *= 1E-4
 
-# Facets in the mesh have tags 0, 1, 2. One is for interfaces between
-# cells and cells and the exterior. Two is used for marking boundary facets
-# of the domain - this is where typically zero DirichletBCs are applied
-# for the potential. The domain is split into 2 subdomains marked as 1 
-# and 2 (cell interior, cell exterior). These differ by conductivities
+# Facets in the mesh have tags 0, 1. One is for interfaces between
+# cells and cells and the exterior. The domain is split into 2 subdomains 
+# marked as 1 and 0 (cell interior, cell exterior). These differ by conductivities
 
-iface_tag, cell_tag = 1, 1
-bdry_tag, ext_tag = 0, 2
+ext_tag, int_tag = 0, 1
+not_iface_tag, iface_tag = 0, 1
 
 surfaces = MeshFunction('size_t', mesh, mesh.topology().dim()-1, 0)
 h5.read(surfaces, 'surfaces')
@@ -72,10 +70,10 @@ p0 = interpolate(Constant(1), Q)
 I_ion = p0
 
 # The system
-a = ((1/cond_int)*inner(sigma, tau)*dx(cell_tag)+(1/cond_ext)*inner(sigma, tau)*dx(ext_tag)
-     - inner(div(tau), u)*dx(cell_tag) - inner(div(tau), u)*dx(ext_tag)
+a = ((1/cond_int)*inner(sigma, tau)*dx(int_tag)+(1/cond_ext)*inner(sigma, tau)*dx(ext_tag)
+     - inner(div(tau), u)*dx(int_tag) - inner(div(tau), u)*dx(ext_tag)
      + inner(p('+'), dot(tau('+'), n('-')))*dS(iface_tag)
-     - inner(div(sigma), v)*dx(cell_tag) - inner(div(sigma), v)*dx(ext_tag)
+     - inner(div(sigma), v)*dx(int_tag) - inner(div(sigma), v)*dx(ext_tag)
      + inner(q('+'), dot(sigma('+'), n('-')))*dS(iface_tag)
      - (C_m/dt_fem)*inner(q('+'), p('+'))*dS(iface_tag))
 
@@ -90,19 +88,19 @@ L += inner(q, I_ion-(C_m/dt_fem)*p0)*ds(iface_tag)
 
 # Additional terms to set to zero the dofs of W.sub(2) which are not on
 # the interfaces
-a -= inner(p('+'), q('+'))*dS(0) + inner(p, q)*ds(bdry_tag)
-L -= inner(Constant(0)('+'), q('+'))*dS(0) + inner(Constant(0), q)*ds(bdry_tag)
+a -= inner(p('+'), q('+'))*dS(not_iface_tag) + inner(p, q)*ds(not_iface_tag)
+L -= inner(Constant(0)('+'), q('+'))*dS(not_iface_tag) + inner(Constant(0), q)*ds(not_iface_tag)
 
 A, b = PETScMatrix(), PETScVector()
 assemble_system(a, L, A_tensor=A, b_tensor=b)
 
 dm = W.sub(2).dofmap().dofs()
 
-# import numpy as np
-# for i in range(A.size(0)):
-#     cols, vals = A.getrow(i)
-#     if np.linalg.norm(vals, 1) == 0:
-#         print (i, vals), i in dm
-# print A.size(0), A.norm('linf')
+import numpy as np
+for i in range(A.size(0)):
+    cols, vals = A.getrow(i)
+    if np.linalg.norm(vals, 1) == 0:
+        print (i, vals), i in dm
+print A.size(0), A.norm('linf')
 
-# print np.sort(np.abs(np.linalg.eigvals(A.array())))[0:20]
+print np.sort(np.abs(np.linalg.eigvals(A.array())))[0:20]
